ee871fc4f7da9989b16841bcde7c7e0e48e7cbc5
张鑫强 <1933581361@qq.com>
2019-08-19 16:43:59 +0800

    使用配置类 版本号：V1.0 模块名：spring03wiring_auto 提交人：zhangxinqiang 类    型：commit 说    明： 使用配置类

--- spring03wiring_auto/src/main/java/soundsystem/App.java
+++ spring03wiring_auto/src/main/java/soundsystem/App.java
@@ -8,10 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
  * @author ：zxq
  * @date ：Created in 2019/8/19 16:31
  */
-@ComponentScan
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         CDPlayer player = context.getBean(CDPlayer.class);
         player.play();
     }
---
+++ spring03wiring_auto/src/main/java/soundsystem/AppConfig.java
@@ -0,0 +1,15 @@
+package soundsystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：zxq
+ * @date ：Created in 2019/8/19 16:41
+ * spring 程序配置类
+ */
+@Configuration
+@ComponentScan
+public class AppConfig {
+
+}
