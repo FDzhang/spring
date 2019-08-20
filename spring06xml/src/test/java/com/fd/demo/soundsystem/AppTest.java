896c6e8fb12c5ad30807502a87b74c980099048b
张鑫强 <1933581361@qq.com>
2019-08-20 14:06:11 +0800

    使用单元测试 版本号：V1.0 模块名：spring06xml 提交人：zhangxinqiang 类    型： 说    明： 使用单元测试

---
+++ spring06xml/src/test/java/com/fd/demo/soundsystem/AppTest.java
@@ -0,0 +1,34 @@
+package com.fd.demo.soundsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author ：zxq
+ * @date ：Created in 2019/8/20 14:00
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class AppTest {
+
+    @Autowired
+    private CompactDisc compactDisc1;
+
+    @Autowired
+    private CompactDisc compactDisc2;
+
+    @Autowired
+    @Qualifier("compactDisc2")
+    private CompactDisc compactDisc3;
+
+    @Test
+    public void test01(){
+        compactDisc1.paly();
+        compactDisc2.paly();
+        compactDisc3.paly();
+    }
+}
