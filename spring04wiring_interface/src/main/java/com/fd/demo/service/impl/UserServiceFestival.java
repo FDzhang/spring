ccd52732f14a4bd5ef0c96f6e93e751fee46f0b7
张鑫强 <1933581361@qq.com>
2019-08-20 09:50:26 +0800

    自动装配-首选bean 版本号：V1.0 模块名：spring04 提交人：zhangxinqiang 类    型： 说    明： 自动装配-首选bean primary

---
+++ spring04wiring_interface/src/main/java/com/fd/demo/service/impl/UserServiceFestival.java
@@ -0,0 +1,18 @@
+package com.fd.demo.service.impl;
+
+import com.fd.demo.service.UserService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ：zxq
+ * @date ：Created in 2019/8/20 9:45
+ */
+
+@Component
+@Primary
+public class UserServiceFestival implements UserService {
+    public void add() {
+        System.out.println("注册用户，发送优惠券");
+    }
+}
